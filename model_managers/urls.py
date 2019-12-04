21cee0bc99b324079f0deec8466453de8b335498
fantaso <fantaso.code@gmail.com>
2019-12-04 14:04:49 +0100

    register the endpoints (urls) for the author book app

--- model_managers/urls.py
+++ model_managers/urls.py
@@ -1,21 +1,13 @@
-"""model_managers URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from author_book.views import AuthorWithCopiesSoldView, AuthorMoreComplexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # annotate_with_copies_sold
+    path('author-list/', AuthorWithCopiesSoldView.as_view()),
+    # combining more methods for a more complex queryset
+    path('author-list-complex/', AuthorMoreComplexView.as_view()),
 ]
