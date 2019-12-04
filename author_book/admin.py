f75ed5389928d26528f56ee45777ef9874a5c9a3
fantaso <fantaso.code@gmail.com>
2019-12-04 14:16:23 +0100

    register the models in the admin app

--- author_book/admin.py
+++ author_book/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from author_book.models import Author, Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'copies_sold', 'author']
